c3993e4329326bb46d32739747cedaed86afc6e4
wagger <dengwx1990@foxmail.com>
2020-03-08 09:19:55 +0800

    first commit

---
+++ rt-thread/include/rtdef.h
@@ -0,0 +1,401 @@
+#ifndef __RT_DEF_H__
+#define __RT_DEF_H__
+
+/* include rtconfig header to import configuration */
+#include <rtconfig.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+
+
+/**
+ * @addtogroup Thread
+ */
+
+/*@{*/
+
+/*
+ * Thread
+ */
+
+/*
+ * thread state definitions
+ */
+#define RT_THREAD_INIT                  0x00
+#define RT_THREAD_READY                 0x01
+#define RT_THREAD_SUSPEND               0x02
+#define RT_THREAD_RUNNING               0x03
+#define RT_THREAD_BLOCK                 RT_THREAD_SUSPEND
+#define RT_THREAD_CLOSE                 0x04
+#define RT_THREAD_STAT_MASK             0x0f
+
+#define RT_THREAD_STAT_SIGNAL           0x10
+#define RT_THREAD_STAT_SIGNAL_READY     (RT_THREAD_STAT_SIGNAL | RT_THREAD_READY)
+#define RT_THREAD_STAT_SIGNAL_WAIT      0x20
+#define RT_THREAD_STAT_SIGNAL_MASK      0xf0
+
+/**
+ * thread control command definitions
+ */
+#define RT_THREAD_CTRL_STARTUP          0x00
+#define RT_THREAD_CTRL_CLOSE            0x01
+#define RT_THREAD_CTRL_CHANGE_PRIORITY  0x02
+#define RT_THREAD_CTRL_INFO             0x03
+
+/**
+ * Thread structure
+ */
+struct rt_thread
+{
+    /* rt object */
+    char        name[RT_NAME_MAX];
+    rt_uint8_t  type;
+    rt_uint8_t  flags;
+
+#ifdef RT_USING_MODULE
+    void       *module_id;
+#endif
+
+    rt_list_t   list;                                   /**< the object list */
+    rt_list_t   tlist;                                  /**< the thread list */
+
+    /* stack point and entry */
+    void       *sp;                                     /**< stack point */
+    void       *entry;
+    void       *parameter;
+    void       *stack_addr;
+    rt_uint32_t stack_size;
+
+    /* error code */
+    rt_err_t    error;
+
+    rt_uint8_t  stat;                                   /**< thread status */
+
+    /* priority */
+    rt_uint8_t  current_priority;
+    rt_uint8_t  init_priority;
+#if RT_THREAD_PRIORITY_MAX > 32
+    rt_uint8_t  number;
+    rt_uint8_t  high_mask;
+#endif
+    rt_uint32_t number_mask;
+
+#if defined(RT_USING_EVENT)
+    /* thread event */
+    rt_uint32_t event_set;
+    rt_uint8_t  event_info;
+#endif
+
+#if defined(RT_USING_SIGNALS)
+    rt_sigset_t     sig_pending;                        /**< the pending signals */
+    rt_sigset_t     sig_mask;                           /**< the mask bits of signal */
+
+    void            *sig_ret;                           /**< the return stack pointer from signal */
+    rt_sighandler_t *sig_vectors;                       /**< vectors of signal handler */
+    void            *si_list;                           /**< the signal infor list */
+#endif
+
+    rt_ubase_t  init_tick;                              /**< thread's initialized tick */
+    rt_ubase_t  remaining_tick;                         /**< remaining tick */
+
+    struct rt_timer thread_timer;                       /**< built-in thread timer */
+
+    void (*cleanup)(struct rt_thread *tid);             /**< cleanup function when thread exit */
+
+    /* light weight process if present */
+#ifdef RT_USING_LWP
+    void         *lwp;
+#endif
+
+    rt_uint32_t user_data;                             /**< private user data beyond this thread */
+};
+typedef struct rt_thread *rt_thread_t;
+
+
+
+
+
+
+
+
+#ifdef RT_USING_DEVICE
+/**
+ * @addtogroup Device
+ */
+
+/*@{*/
+
+/**
+ * device (I/O) class type
+ */
+enum rt_device_class_type
+{
+    RT_Device_Class_Char = 0,
+    RT_Device_Class_Block,
+    RT_Device_Class_NetIf,
+    RT_Device_Class_MTD,
+    RT_Device_Class_CAN,
+    RT_Device_Class_RTC,
+    RT_Device_Class_Sound,
+    RT_Device_Class_Graphic,
+    RT_Device_Class_I2CBUS,
+    RT_Device_Class_USBDevice,
+    RT_Device_Class_USBHost,
+    RT_Device_Class_SPIBUS,
+    RT_Device_Class_SPIDevice,
+    RT_Device_Class_SDIO,
+    RT_Device_Class_PM,
+    RT_Device_Class_Pipe,
+    RT_Device_Class_Portal,
+    RT_Device_Class_Timer,
+    RT_Device_Class_Miscellaneous,
+    RT_Device_Class_Unknown,
+};
+
+/**
+ * device flags defitions
+ */
+#define RT_DEVICE_FLAG_DEACTIVATE       0x0000
+
+#define RT_DEVICE_FLAG_RDONLY           0x0001
+#define RT_DEVICE_FLAG_WRONLY           0x0002
+#define RT_DEVICE_FLAG_RDWR             0x0003
+
+#define RT_DEVICE_FLAG_REMOVABLE        0x0004
+#define RT_DEVICE_FLAG_STANDALONE       0x0008
+#define RT_DEVICE_FLAG_ACTIVATED        0x0010
+#define RT_DEVICE_FLAG_SUSPENDED        0x0020
+#define RT_DEVICE_FLAG_STREAM           0x0040
+
+#define RT_DEVICE_FLAG_INT_RX           0x0100
+#define RT_DEVICE_FLAG_DMA_RX           0x0200
+#define RT_DEVICE_FLAG_INT_TX           0x0400
+#define RT_DEVICE_FLAG_DMA_TX           0x0800
+
+#define RT_DEVICE_OFLAG_CLOSE           0x0000
+#define RT_DEVICE_OFLAG_RDONLY          0x0001
+#define RT_DEVICE_OFLAG_WRONLY          0x0002
+#define RT_DEVICE_OFLAG_RDWR            0x0003
+#define RT_DEVICE_OFLAG_OPEN            0x0008
+#define RT_DEVICE_OFLAG_MASK            0x0f0f
+
+/**
+ * general device commands
+ */
+#define RT_DEVICE_CTRL_RESUME           0x01
+#define RT_DEVICE_CTRL_SUSPEND          0x02
+#define RT_DEVICE_CTRL_CONFIG           0x03
+
+#define RT_DEVICE_CTRL_SET_INT          0x10
+#define RT_DEVICE_CTRL_CLR_INT          0x11
+#define RT_DEVICE_CTRL_GET_INT          0x12
+
+/**
+ * special device commands
+ */
+#define RT_DEVICE_CTRL_CHAR_STREAM      0x10
+#define RT_DEVICE_CTRL_BLK_GETGEOME     0x10
+#define RT_DEVICE_CTRL_BLK_SYNC         0x11
+#define RT_DEVICE_CTRL_BLK_ERASE        0x12
+#define RT_DEVICE_CTRL_BLK_AUTOREFRESH  0x13
+#define RT_DEVICE_CTRL_NETIF_GETMAC     0x10
+#define RT_DEVICE_CTRL_MDT_FORMAT       0x10
+#define RT_DEVICE_CTRL_RTC_GET_TIME     0x10
+#define RT_DEVICE_CTRL_RTC_SET_TIME     0x11
+#define RT_DEVICE_CTRL_RTC_GET_ALARM    0x12
+#define RT_DEVICE_CTRL_RTC_SET_ALARM    0x13
+
+typedef struct rt_device *rt_device_t;
+
+/**
+ * operations set for device object
+ */
+struct rt_device_ops
+{
+    /* common device interface */
+    rt_err_t  (*init)    (rt_device_t dev);
+    rt_err_t  (*open)    (rt_device_t dev, rt_uint16_t oflag);
+    rt_err_t  (*close)   (rt_device_t dev);
+    rt_size_t (*read)    (rt_device_t dev, rt_off_t pos, void *buffer, rt_size_t size);
+    rt_size_t (*write)   (rt_device_t dev, rt_off_t pos, const void *buffer, rt_size_t size);
+    rt_err_t  (*control) (rt_device_t dev, int cmd, int *args);
+};
+
+/**
+ * WaitQueue structure
+ */
+struct rt_wqueue
+{
+    rt_uint32_t flag;
+    rt_list_t waiting_list;
+};
+typedef struct rt_wqueue *rt_wqueue_t;
+
+/**
+ * Device structure
+ */
+struct rt_device
+{
+    struct rt_object          parent;
+
+    enum rt_device_class_type type;
+    rt_uint16_t               flag;
+    rt_uint16_t               open_flag;
+
+    rt_uint8_t                ref_count;
+    rt_uint8_t                device_id;                /**< 0 - 255 */
+
+    /* device call back */
+    rt_err_t (*rx_indicate) (rt_device_t dev, rt_size_t size);
+    rt_err_t (*tx_complete) (rt_device_t dev, void *buffer);
+
+#ifdef RT_USING_DEVICE_OPS
+    const struct rt_device_ops *ops;
+#else
+    /* common device interface */
+    rt_err_t  (*init)    (rt_device_t dev);
+    rt_err_t  (*open)    (rt_device_t dev, rt_uint16_t oflag);
+    rt_err_t  (*close)   (rt_device_t dev);
+    rt_size_t (*read)    (rt_device_t dev, rt_off_t pos, void *buffer, rt_size_t size);
+    rt_size_t (*write)   (rt_device_t dev, rt_off_t pos, const void *buffer, rt_size_t size);
+    rt_err_t  (*control) (rt_device_t dev, int cmd, int *args);
+#endif
+
+#if defined (RT_USING_POSIX)
+    const struct dfs_file_ops *fops;
+    struct rt_wqueue wait_queue;
+#endif
+
+    void                      *user_data;
+};
+
+/**
+ * block device geometry structure
+ */
+struct rt_device_blk_geometry
+{
+    rt_uint32_t sector_count;
+    rt_uint32_t bytes_per_sector;
+    rt_uint32_t block_size;
+};
+
+/**
+ * sector arrange struct on block device
+ */
+struct rt_device_blk_sectors
+{
+    rt_uint32_t sector_begin;
+    rt_uint32_t sector_end;
+};
+
+/**
+ * cursor control command
+ */
+#define RT_DEVICE_CTRL_CURSOR_SET_POSITION   0x10
+#define RT_DEVICE_CTRL_CURSOR_SET_TYPE       0x11
+
+/**
+ * graphic device control command
+ */
+#define RTGRAPHIC_CTRL_RECT_UPDATE           0
+#define RTGRAPHIC_CTRL_POWERON               1
+#define RTGRAPHIC_CTRL_POWEROFF              2
+#define RTGRAPHIC_CTRL_GET_INFO              3
+#define RTGRAPHIC_CTRL_MODE                  4
+#define RTGRAPHIC_CTRL_EXT                   5
+
+/* graphic deice */
+enum
+{
+    RTGRAPHIC_PIXEL_FORMAT_MONO = 0,
+    RTGRAPHIC_PIXEL_FORMAT_GRAY4,
+    RTGRAPHIC_PIXEL_FORMAT_GRAY16,
+    RTGRAPHIC_PIXEL_FORMAT_RGB332,
+    RTGRAPHIC_PIXEL_FORMAT_RGB444,
+    RTGRAPHIC_PIXEL_FORMAT_RGB565,
+    RTGRAPHIC_PIXEL_FORMAT_RGB565P,
+    RTGRAPHIC_PIXEL_FORMAT_BGR565 = RTGRAPHIC_PIXEL_FORMAT_RGB565P,
+    RTGRAPHIC_PIXEL_FORMAT_RGB666,
+    RTGRAPHIC_PIXEL_FORMAT_RGB888,
+    RTGRAPHIC_PIXEL_FORMAT_ARGB888,
+    RTGRAPHIC_PIXEL_FORMAT_ABGR888,
+    RTGRAPHIC_PIXEL_FORMAT_ARGB565,
+    RTGRAPHIC_PIXEL_FORMAT_ALPHA,
+};
+
+/**
+ * build a pixel position according to (x, y) coordinates.
+ */
+#define RTGRAPHIC_PIXEL_POSITION(x, y)    ((x << 16) | y)
+
+/**
+ * graphic device information structure
+ */
+struct rt_device_graphic_info
+{
+    rt_uint8_t  pixel_format;
+    rt_uint8_t  bits_per_pixel;
+    rt_uint16_t reserved;
+
+    rt_uint16_t width;
+    rt_uint16_t height;
+
+    rt_uint8_t  *framebuffer;
+};
+
+/**
+ * rectangle information structure
+ */
+struct rt_device_rect_info
+{
+    rt_uint16_t x;
+    rt_uint16_t y;
+    rt_uint16_t width;
+    rt_uint16_t height;
+};
+
+/**
+ * graphic operations
+ */
+struct rt_device_graphic_ops
+{
+    void (*set_pixel)  (const char *pixel, int x, int y);
+    void (*get_pixel)  (char *pixel, int x, int y);
+    
+    void (*draw_hline) (const char *pixel, int x1, int x2, int y);
+    void (*draw_vline) (const char *pixel, int x, int y1, int y2);
+
+    void (*blit_line)  (const char *pixel, int x, int y, rt_size_t size);
+};
+#define rt_graphic_ops(device)     ((struct rt_device_graphic_ops *)(device->user_data))
+
+/*@}*/
+#endif /* end of RT_USING_DEVICE */
+
+/* definitions for libc */
+#include "rtlibc.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#ifdef __cplusplus
+/* RT-Thread definitions for C++ */
+namespace rtthread {
+
+enum TICK_WAIT {
+    WAIT_NONE = 0,
+    WAIT_FOREVER = -1,
+};
+
+}
+
+#endif /* end of __cplusplus */
+
+#endif
---
+++ rt-thread/src/thread.c
@@ -0,0 +1,472 @@
+#include <rtthread.h>
+#include <rthw.h>
+
+extern rt_list_t rt_thread_priority_table[RT_THREAD_PRIORITY_MAX];
+extern struct rt_thread *rt_current_thread;
+extern rt_list_t rt_thread_defunct;
+
+#ifdef RT_USING_HOOK
+
+static void (*rt_thread_suspend_hook)(rt_thread_t thread);
+static void (*rt_thread_resume_hook) (rt_thread_t thread);
+static void (*rt_thread_inited_hook) (rt_thread_t thread);
+
+/**
+ * @ingroup Hook
+ * This function sets a hook function when the system suspend a thread.
+ *
+ * @param hook the specified hook function
+ *
+ * @note the hook function must be simple and never be blocked or suspend.
+ */
+void rt_thread_suspend_sethook(void (*hook)(rt_thread_t thread))
+{
+    rt_thread_suspend_hook = hook;
+}
+
+/**
+ * @ingroup Hook
+ * This function sets a hook function when the system resume a thread.
+ *
+ * @param hook the specified hook function
+ *
+ * @note the hook function must be simple and never be blocked or suspend.
+ */
+void rt_thread_resume_sethook(void (*hook)(rt_thread_t thread))
+{
+    rt_thread_resume_hook = hook;
+}
+
+/**
+ * @ingroup Hook
+ * This function sets a hook function when a thread is initialized.
+ *
+ * @param hook the specified hook function
+ */
+void rt_thread_inited_sethook(void (*hook)(rt_thread_t thread))
+{
+    rt_thread_inited_hook = hook;
+}
+
+#endif
+
+void rt_thread_exit(void)
+{
+    struct rt_thread *thread;
+    register rt_base_t level;
+
+    /* get current thread */
+    thread = rt_current_thread;
+
+    /* disable interrupt */
+    level = rt_hw_interrupt_disable();
+
+    /* remove from schedule */
+    rt_schedule_remove_thread(thread);
+
+    /* change stat */
+    thread->stat = RT_THREAD_CLOSE;
+
+    /* remove it from timer list */
+    rt_timer_detach(&thread->thread_timer);
+
+    if ((rt_object_is_systemobject((rt_object_t)thread) == RT_TRUE) &&
+        thread->cleanup == RT_NULL)
+    {
+        rt_object_detach((rt_object_t)thread);
+    }
+    else
+    {
+        /* insert to defunct thread list */
+        rt_list_insert_after(&rt_thread_defunct, &(thread->tlist));
+    }
+
+    /* enable interrupt */
+    rt_hw_interrupt_enable(level);
+
+    /* switch to next task */
+    rt_schedule();
+}
+
+static rt_err_t _rt_thread_init(struct rt_thread *thread,
+                                const char       *name,
+                                void (*entry)(void *parameter),
+                                void             *parameter,
+                                void             *stack_start,
+                                rt_uint32_t       stack_size,
+                                rt_uint8_t        priority,
+                                rt_uint32_t       tick)
+{
+    /* init thread list */
+    rt_list_init(&(thread->tlist));
+
+    thread->entry = (void *)entry;
+    thread->parameter = parameter;
+
+    /* stack init */
+    thread->stack_addr = stack_start;
+    thread->stack_size = stack_size;
+
+    /* init thread stack */
+    rt_memset(thread->stack_addr, '#', thread->stack_size);
+    thread->sp = (void *)rt_hw_stack_init(thread->entry, 
+                                          thread->parameter,
+                                          (void *)((char *)thread->stack_addr + thread->stack_size - 4),
+                                          (void *)rt_thread_exit);
+
+    /* priority init */
+    RT_ASSERT(priority < RT_THREAD_PRIORITY_MAX);
+    thread->init_priority    = priority;
+    thread->current_priority = priority;
+
+    thread->number_mask = 0;
+#if RT_THREAD_PRIORITY_MAX > 32
+    thread->number      = 0;
+    thread->high_mask   = 0;
+#endif
+
+    /* tick init */
+    thread->init_tick      = tick;
+    thread->remaining_tick = tick;
+
+    /* error and flags */
+    thread->error = RT_EOK;
+    thread->stat  = RT_THREAD_INIT;
+
+    /* initialize cleanup function and user data */
+    thread->cleanup   = 0;
+    thread->user_data = 0;
+
+    /* init thread timer */
+    rt_timer_init(&(thread->thread_timer),
+                  thread->name,
+                  rt_thread_timeout,
+                  thread,
+                  0,
+                  RT_TIMER_FLAG_ONE_SHOT);
+
+    /* initialize signal */
+#ifdef RT_USING_SIGNALS
+    thread->sig_mask    = 0x00;
+    thread->sig_pending = 0x00;
+
+    thread->sig_ret     = RT_NULL;
+    thread->sig_vectors = RT_NULL;
+    thread->si_list     = RT_NULL;
+#endif
+
+#ifdef RT_USING_LWP
+    thread->lwp = RT_NULL；
+#endif
+
+    RT_OBJECT_HOOK_CALL(rt_thread_inited_hook, (thread));
+
+    return RT_EOK;
+}
+
+/**
+ * @addtogroup Thread
+ */
+
+/**@{*/
+
+/**
+ * This function will initialize a thread, normally it's used to initialize a
+ * static thread object.
+ *
+ * @param thread the static thread object
+ * @param name the name of thread, which shall be unique
+ * @param entry the entry function of thread
+ * @param parameter the parameter of thread enter function
+ * @param stack_start the start address of thread stack
+ * @param stack_size the size of thread stack
+ * @param priority the priority of thread
+ * @param tick the time slice if there are same priority thread
+ *
+ * @return the operation status, RT_EOK on OK, -RT_ERROR on error
+ */
+rt_err_t rt_thread_init(struct rt_thread *thread,
+                        const char       *name,
+                        void (*entry)(void *parameter),
+                        void             *parameter,
+                        void             *stack_start,
+                        rt_uint32_t       stack_size,
+                        rt_uint32_t       priority,
+                        rt_uint32_t       tick)
+{
+    /* thread check */
+    RT_ASSERT(thread != RT_NULL);
+    RT_ASSERT(stack_start != RT_NULL);
+
+    /* init thread object */
+    rt_object_init((rt_object_t)thread, RT_Object_Class_Thread, name);
+
+    return _rt_thread_init(thread,
+                           name,
+                           entry,
+                           parameter,
+                           stack_start,
+                           stack_size,
+                           priority,
+                           tick);
+}
+RTM_EXPORT(rt_thread_init);
+
+/**
+ * This function will return self thread object
+ *
+ * @return the self thread object
+ */
+rt_thread_t rt_thread_self(void)
+{
+    return rt_current_thread;
+}
+RTM_EXPORT(rt_thread_self);
+
+/**
+ * This function will start a thread and put it to system ready queue
+ *
+ * @param thread the thread to be started
+ *
+ * @return the operation status, RT_EOK on OK, -RT_ERROR on error
+ */
+rt_err_t rt_thread_startup(rt_thread_t thread)
+{
+    /* thread check */
+    RT_ASSERT(thread != RT_NULL);
+    RT_ASSERT((thread->stat & RT_THREAD_STAT_MASK) == RT_THREAD_INIT);
+    RT_ASSERT(rt_object_get_type((rt_object_t)thread) == RT_Object_Class_Thread);
+
+    /* set current priority to init priority */
+    thread->current_priority = thread->init_priority;
+
+    /* calculate priority attribute */
+#if RT_THREAD_PRIORITY_MAX > 32
+    thread->number      = thread->current_priority >> 3;            /* 5bit */
+    thread->number_mask = 1L << thread->number;
+    thread->high_mask   = 1L << (thread->current_priority & 0x07);  /* 3bit */
+#else
+    thread->number_mask = 1L << thread->current_priority;
+#endif
+
+    RT_DEBUG_LOG(RT_DEBUG_THREAD, ("startup a thread:%s with priority:%d\n",
+                                   thread->name, thread->init_priority));
+
+    /* change thread stat */
+    thread->stat = RT_THREAD_SUSPEND;
+    /* then resume it */
+    rt_thread_resume(thread);
+    if (rt_thread_self() != RT_NULL)
+    {
+        /* do a scheduling */
+        rt_schedule();
+    }
+    
+    return RT_EOK；
+}
+RTM_EXPORT(rt_thread_startup);
+
+/**
+ * This function will detach a thread. The thread object will be removed from
+ * thread queue and detached/deleted from system object management.
+ *
+ * @param thread the thread to be deleted
+ *
+ * @return the operation status, RT_EOK on OK, -RT_ERROR on error
+ */
+rt_err_t rt_thread_detach(rt_thread_t thread)
+{
+    rt_base_t lock;
+
+    /* thread check */
+    RT_ASSERT(thread != RT_NULL);
+    RT_ASSERT(rt_object_get_type((rt_object_t)thread) == RT_Object_Class_Thread);
+    RT_ASSERT(rt_object_is_systemobject((rt_object_t)thread));
+
+    if ((thread->stat & RT_THREAD_STAT_MASK) != RT_THREAD_INIT)
+    {
+        /* remove from schedule */
+        rt_schedule_remove_thread(thread);
+    }
+
+    /* release thread timer */
+    rt_timer_detach(&(thread->thread_timer));
+
+    /* change stat */
+    thread->stat = RT_THREAD_CLOSE;
+
+    /* detach object */
+    rt_object_detach((rt_object_t)thread);
+
+    if (thread->cleanup != RT_NULL)
+    {
+        /* disable interrupt */
+        lock = rt_hw_interrupt_disable();
+
+        /* insert to defunct thread list */
+        rt_list_insert_after(&rt_thread_defunct, &(thread->tlist));
+
+        /* enable interrupt */
+        rt_hw_interrupt_enable(lock);
+    }
+
+    return RT_EOK;
+}
+RTM_EXPORT(rt_thread_detach);
+
+#ifdef RT_USING_HEAP
+/**
+ * This function will create a thread object and allocate thread object memory
+ * and stack.
+ *
+ * @param name the name of thread, which shall be unique
+ * @param entry the entry function of thread
+ * @param parameter the parameter of thread enter function
+ * @param stack_size the size of thread stack
+ * @param priority the priority of thread
+ * @param tick the time slice if there are same priority thread
+ *
+ * @return the created thread object
+ */
+rt_thread_t rt_thread_create(const char *name,
+                             void (*entry)(void *parameter),
+                             void       *parameter,
+                             rt_uint32_t stack_size,
+                             rt_uint8_t  priority,
+                             rt_uint32_t tick)
+{
+    struct rt_thread *thread;
+    void *stack_start;
+
+    thread = (struct rt_thread *)rt_object_allocate(RT_Object_Class_Thread, 
+                                                    name);
+    if(thread == RT_NULL)
+        return RT_NULL;
+
+    stack_start = (void *)RT_KERNAL_MALLOC(stack_size);
+    if (stack_start == RT_NULL)
+    {
+        /* allocate stack failure */
+        rt_object_delete((rt_object_t)thread);
+
+        return RT_NULL;
+    }
+
+    _rt_thread_init(thread,
+                    name,
+                    entry,
+                    parameter,
+                    stack_start,
+                    stack_size,
+                    priority,
+                    tick);
+
+    return thread;
+}
+RTM_EXPORT(rt_thread_create);
+
+/**
+ * This function will delete a thread. The thread object will be removed from
+ * thread queue and deleted from system object management in the idle thread.
+ *
+ * @param thread the thread to be deleted
+ *
+ * @return the operation status, RT_EOK on OK, -RT_ERROR on error
+ */
+rt_err_t rt_thread_delete(rt_thread_t thread)
+{
+    rt_base_t lock;
+
+    /* thread check */
+    RT_ASSERT(thread != RT_NULL);
+    RT_ASSERT(rt_object_get_type((rt_object_t)thread) == RT_Object_Class_Thread);
+    RT_ASSERT(rt_object_is_systemobject((rt_object_t)thread) == RT_FALSE);
+
+    if ((thread->stat & RT_THREAD_STAT_MASK) != RT_THREAD_INIT)
+    {
+        /* remove from schedule */
+        rt_schedule_remove_thread(thread);
+    }
+
+    /* release thread timer */
+    rt_timer_detach(&(thread->thread_timer));
+
+    /* change stat */
+    thread->stat = RT_THREAD_CLOSE;
+
+    /* disable interrupt */
+    lock = rt_hw_interrupt_disable();
+
+    /* insert to defunct thread list */
+    rt_list_insert_after(&rt_thread_defunct, &(thread->tlist));
+
+    /* enable interrupt */
+    rt_hw_interrupt_enable(lock);
+
+    return RT_EOK;
+}
+RTM_EXPORT(rt_thread_delete);
+#endif
+
+/**
+ * This function will let current thread yield processor, and scheduler will
+ * choose a highest thread to run. After yield processor, the current thread
+ * is still in READY state.
+ *
+ * @return RT_EOK
+ */
+rt_err_t rt_threead_yield(void)
+{
+    register rt_base_t level;
+    struct rt_thread *thread;
+
+    /* disable interrupt */
+    levle = rt_hw_interrupt_disable();
+
+    /* set to current thread */
+    thread = rt_current_thread;
+
+    /* if the thread stat is READY and on ready queue list */
+    if ((thread->stat & RT_THREAD_STAT_MASK) == RT_THREAD_READY &&
+        thread->tlist.next != thread->tlist.prev)
+    {
+        /* remove thread from thread list */
+        rt_list_remove(&(thread->tlist));
+
+        /* put thread to end of ready queue */
+        rt_list_insert_before(&(rt_thread_priority_table[thread->current_priority]),
+                              &(thread->tlist));
+
+        /* enable interrupt */
+        rt_hw_interrupt_enable(level);
+
+        rt_schedule();
+
+        return RT_EOK;
+    }
+
+    /* enable interrupt */
+    rt_hw_interrupt_enable(level);
+    
+    return RT_EOK;
+}
+RTM_EXPORT(rt_thread_yield);
+
+/**
+ * This function will let current thread sleep for some ticks.
+ *
+ * @param tick the sleep ticks
+ *
+ * @return RT_EOK
+ */
+rt_err_t rt_thread_sleep(rt_tick_t tick)
+{
+    /* disable interrupt */
+    /* set to current thread */
+    /* suspend thread */
+    /* reset the timeout of thread timer and start it */
+    /* enable interrupt */
+    /* clear error number of this thread to RT_EOK */
+    
+    return RT_EOK;
+}
