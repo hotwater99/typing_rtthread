a8b6bd94ad480ed0fb8cb792dcf6542c6b721ab0
hotwater99 <dengwx1990@foxmail.com>
2020-03-26 07:07:57 +0800

    Update ipc.c
    add "mutex" related functions

--- Source/rt-thread/src/ipc.c
+++ Source/rt-thread/src/ipc.c
@@ -478,15 +478,445 @@ rt_err_t rt_sem_control(rt_sem_t sem, int cmd, void *arg)
 RTM_EXPORT(rt_sem_control);
 #endif /* end of RT_USING_SEMAPHORE */
 
+#ifdef RT_USING_MUTEX
+/**
+ * This function will initialize a mutex and put it under control of resource
+ * management.
+ *
+ * @param mutex the mutex object
+ * @param name the name of mutex
+ * @param flag the flag of mutex
+ *
+ * @return the operation status, RT_EOK on successful
+ */
+rt_err_t rt_mutex_init(rt_mutex_t mutex, const char *name, rt_uint8_t flag)
+{
+    /* parameter check */
+    RT_ASSERT(mutex != RT_NULL);
 
+    /* init object */
+    rt_object_init(&(mutex->parent.parent), RT_Object_Class_Mutex, name);
 
+    /* init ipc object */
+    rt_ipc_object_init(&(mutex->parent));
 
+    mutex->value = 1;
+    mutex->owner = RT_NULL;
+    mutex->original_priority = 0xFF;
+    mutex->hold  = 0;
 
+    /* set flag */
+    mutex->parent.parent.flag = flag;
 
+    return RT_EOK;
+}
+RTM_EXPORT(rt_mutex_init);
 
+/**
+ * This function will detach a mutex from resource management
+ *
+ * @param mutex the mutex object
+ *
+ * @return the operation status, RT_EOK on successful
+ *
+ * @see rt_mutex_delete
+ */
+rt_err_t rt_mutex_detach(rt_mutex_t mutex)
+{
+    /* parameter check */
+    RT_ASSERT(mutex != RT_NULL);
+    RT_ASSERT(rt_object_get_type(&mutex->parent.parent) == RT_Object_Class_Mutex);
+    RT_ASSERT(rt_object_is_systemobject(&mutex->parent.parent));
 
+    /* wakeup all suspend threads */
+    rt_ipc_list_resume_all(&(mutex->parent.suspend_thread));
 
+    /* detach semaphore object */
+    rt_object_detach(&(mutex->parent.parent));
 
+    return RT_EOK;
+}
+RTM_EXPORT(rt_mutex_detach);
 
+#ifdef RT_USING_HEAP
+/**
+ * This function will create a mutex from system resource
+ *
+ * @param name the name of mutex
+ * @param flag the flag of mutex
+ *
+ * @return the created mutex, RT_NULL on error happen
+ *
+ * @see rt_mutex_init
+ */
+rt_mutex_t rt_mutex_create(const char *name, rt_uint8_t flag)
+{
+    struct rt_mutex *mutex;
 
+    RT_DEBUG_NOT_IN_INTERRUPT;
 
+    /* allocate object */
+    mutex = (rt_mutex_t)rt_object_allocate(RT_Object_Class_Mutex, name);
+    if (mutex == RT_NULL)
+        return mutex;
+
+    /* init ipc object */
+    rt_ipc_object_init(&(mutex->parent));
+
+    mutex->value             = 1;
+    mutex->owner             = RT_NULL;
+    mutex->original_priority = 0xFF;
+    mutex->hold              = 0;
+
+    /* set flag */
+    mutex->parent.parent.flag = flag;
+
+    return mutex;
+}
+RTM_EXPORT(rt_mutex_create);
+
+/**
+ * This function will delete a mutex object and release the memory
+ *
+ * @param mutex the mutex object
+ *
+ * @return the error code
+ *
+ * @see rt_mutex_detach
+ */
+rt_err_t rt_mutex_delete(rt_mutex_ mutex)
+{
+    RT_DEBUG_NOT_IN_INTERRUPT;
+
+    /* parameter check */
+    RT_ASSERT(mutex != RT_NULL);
+    RT_ASSERT(rt_object_get_type(&mutex->parent.parent) == RT_Object_Class_Mutex);
+    RT_ASSERT(rt_object_is_systemobject(&mutex->parent.parent) == RT_FALSE);
+
+    /* wakeup all suspend threads */
+    rt_ipc_list_resume_all(&(mutex->parent.suspend_thread));
+
+    /* delete semaphore object */
+    rt_object_delete(&(mutex->parent.parent));
+
+    return RT_EOK;
+}
+RTM_EXPORT(rt_mutex_delete);
+#endif
+
+/**
+ * This function will take a mutex, if the mutex is unavailable, the
+ * thread shall wait for a specified time.
+ *
+ * @param mutex the mutex object
+ * @param time the waiting time
+ *
+ * @return the error code
+ */
+rt_err_t rt_mutex_take(rt_mutex_t mutex, rt_int32_t time)
+{
+    register rt_base_t temp;
+    struct rt_thread *thread;
+
+    /* this function must not be used in interrupt even if time = 0 */
+    RT_DEBUG_IN_THREAD_CONTEXT;
+
+    /* parameter check */
+    RT_ASSERT(mutex != RT_NULL);
+    RT_ASSERT(rt_object_get_type(&mutex->parent.parent) == RT_Object_Class_Mutex);
+
+    /* get current thread */
+    thread = rt_thread_self();
+
+    /* disable interrupt */
+    temp = rt_hw_interrupt_disabled();
+
+    RT_OBJECT_HOOK_CALL(rt_object_trytake_hook, (&(mutex->parent.parent)));
+
+    RT_DEBUG_LOG(RT_DEBUG_IPC,
+                 ("mutex take: current thread: %s, mutex value: %d, hold: %d\n",
+                 thread->name, mutex->value, mutex->hold));
+
+    /* reset thread error */
+    thread->error = RT_EOK;
+
+    if (mutex->owner == thread)
+    {
+        /* it's the same thread */
+        mutex->hold ++;
+    }
+    else
+    {
+__again:
+        /* The value of mutex is 1 in initial status. Therefore, if the
+         * value is great than 0, it indicates the mutex is avaible.
+         */
+        if (mutex->value > 0)
+        {
+            /* mutex is available */
+            mutex->value --;
+
+            /* set mutex owner and original priority */
+            mutex->owner            = thread;
+            mutex->original_priority = thread->current_priority;
+            mutex->hold ++;
+        }
+        else
+        {
+            /* no waiting, return with timeout */
+            if (time == 0)
+            {
+                /* set error as timeout */
+                thread->error = -RT_ETIMEOUT;
+
+                /* enable interrupt */
+                rt_hw_interrupt_enabled(temp);
+
+                return -RT_ETIMEOUT;
+            }
+            else
+            {
+                /* mutex is unavailable, push to suspend list */
+                RT_DEBUG_LOG(RT_DEBUG_IPC, ("mutex_take: suspend thread: %s\n",
+                                            thread->name));
+
+                /* change the owner thread priority of mutex */
+                if (thread->current_priority < mutex->ower->current_priority)
+                {
+                    /* change the owner thread priority */
+                    rt_thread_control(mutex->owner,
+                                      RT_THREAD_CTRL_CHANGE_PRORITY,
+                                      &thread->current_priority);
+                }
+
+                /* suspend current thread */
+                rt_ipc_list_suspend(&(mutex->parent.suspend_thread),
+                                    thread,
+                                    mutex->parent.parent.flag);
+
+                /* has waiting time, start thread timer */
+                if (time > 0)
+                {
+                    RT_DEBUG_LOG(RT_DEBUG_IPC,
+                                 "mutex_take: start the timer of thread: %s\n",
+                                 thread->name);
+
+                    /* reset the timeout of thread timer and start it */
+                    rt_timer_control(&(thread->thread_timer),
+                                     RT_TIMER_CTRL_SET_TIME,
+                                     &time);
+                    rt_timer_start(&(thread->thread_timer));
+                }
+
+                /* enable interrupt */
+                rt_hw_interrupt_enabled(temp);
+
+                /* do schedule */
+                rt_schedule();
+
+                if (thread->error != RT_EOK)
+                {
+                    /* interrupt by signal, try it again */
+                    if (thread->error == -RT_EINTR) goto __again;
+
+                    /* return error */
+                    return thread->error;
+                }
+                else
+                {
+                    /* the mutex is taken successfully. */
+                    /* disable interrupt */
+                    temp = rt_hw_interrupt_disabled();
+                }
+            }
+        }
+    }
+
+    /* enable interrupt */
+    rt_hw_interrupt_enabled(temp);
+
+    RT_OBJECT_HOOK_CALL(rt_object_take_hook, (&(mutex->parent.parent)));
+
+    return RT_EOK;
+}
+RTM_EXPORT(rt_mutex_take);
+
+/**
+ * This function will release a mutex, if there are threads suspended on mutex,
+ * it will be waked up.
+ *
+ * @param mutex the mutex object
+ *
+ * @return the error code
+ */
+rt_err_t rt_mutex_release(rt_mutex_t mutex)
+{
+    register rt_base_t temp;
+    struct rt_thread *thread;
+    rt_bool_t need_schedule;
+
+    /* parameter check */
+    RT_ASSERT(mutex != RT_NULL);
+    RT_ASSERT(rt_object_get_type(&mutex->parent.parent) == RT_Object_Class_Mutex);
+
+    need_schedule = RT_FALSE;
+
+    /* only thread could release mutex because we need test the ownership */
+    RT_DEBUG_IN_THREAD_CONTEXT;
+
+    /* get current thread */
+    thread = rt_thread_self();
+
+    /* disable interrupt */
+    temp = rt_hw_interrupt_disabled();
+
+    RT_DEBUG_LOG(RT_DEBUG_IPC,
+                 ("mutex_release: current thread %s, mutex value: %d, hold: %d\n",
+                  thread->name, mutex->value, mutex->hold));
+
+    RT_OBJECT_HOOK_CALL(rt_object_put_hook, (&(mutex->parent.parent)));
+
+    /* mutex only can be released by owner */
+    if (thread != mutex->owner)
+    {
+        thread->error = -RT_ERROR;
+
+        /* enable interrupt */
+        rt_hw_interrupt_enabled(temp);
+
+        return -RT_ERROR;
+    }
+
+    /* decrease hold */
+    if (thread->hold > 0) thread->hold --;
+    /* if no hold */
+    if (mutex->hold == 0)
+    {
+        /* change the owner thread to original priority */
+        if (mutex->original_priority != mutex->current_priority)
+        {
+            rt_thread_control(mutex->owner,
+                              RT_THREAD_CTRL_CHANGE_PRORITY,
+                              &(mutex->original_priority));
+        }
+
+        /* wakeup suspended thread */
+        if (!rt_list_isempty(&mutex->parent.suspend_thread))
+        {
+            /* get suspended thread */
+            thread = rt_list_entry(mutex->parent.suspend_thread.next,
+                                   struct rt_thread,
+                                   tlist);
+
+            RT_DEBUG_LOG(RT_DEBUG_IPC, ("mutex_release: resume thread: %s\n",
+                                        thread->name));
+
+            /* set new owner and priority */
+            mutex->owner             = thread;
+            mutex->original_priority = thread->current_priority;
+            mutex->hold ++;
+
+            /* resume thread */
+            rt_ipc_list_resume(&(mutex->parent.suspend_thread));
+
+           need_schedule = RT_TRUE; 
+        }
+        else
+        {
+            /* increase value */
+            mutex->value ++;
+
+            /* clear owner */
+            mutex->owner = RT_NULL;
+            mutex->original_priority = 0xff;
+        }
+    }
+
+    /* enable interrupt */
+    rt_hw_interrupt_enabled(temp);
+
+    /* perform a schedule */
+    if (need_schedule = RT_TRUE)
+        rt_schedule();
+
+    return RT_EOK;
+}
+RTM_EXPORT(rt_mutex_release);
+
+/**
+ * This function can get or set some extra attributions of a mutex object.
+ *
+ * @param mutex the mutex object
+ * @param cmd the execution command
+ * @param arg the execution argument
+ *
+ * @return the error code
+ */
+rt_err_t rt_mutex_control(rt_mutex_t mutex, int cmd, void *arg)
+{
+    /* parameter check */
+    RT_ASSERT(mutex != RT_NULL);
+    RT_ASSERT(rt_object_get_type(&mutex->parent.parent) == RT_Object_Class_Mutex);
+
+    return -RT_ERROR;
+}
+RTM_EXPORT(rt_mutex_control);
+#endif /* end of RT_USING_MUTEX */
+
+#ifdef RT_USING_EVENT
+rt_err_t rt_event_init(rt_event_t event, const char *name, rt_uint8_t flag);
+rt_err_t rt_event_detach(rt_event_t event);
+rt_event_t rt_event_create(const char *name, rt_uint8_t flag);
+rt_err_t rt_event_delete(rt_event_t event);
+
+rt_err_t rt_event_send(rt_event_t event, rt_int32_t set);
+rt_err_t rt_event_recv(rt_event_t   event,
+                       rt_uint32_t  set,
+                       rt_uint8_t   opt,
+                       rt_int32_t   timeout,
+                       rt_uint32_t *recved);
+rt_err_t rt_event_control(rt_event_t event, int cmd, void *arg);
+#endif /* end of RT_USING_EVENT */
+
+#ifdef RT_USING_MAILBOX
+rt_err_t rt_mb_init(rt_mailbox_t mb,
+                    const char  *name,
+                    void        *msgpool,
+                    rt_size_t    size,
+                    rt_uint8_t   flag);
+rt_err_t rt_mb_detach(rt_mailbox_t mb);
+rt_mailbox_t rt_mb_create(const char *name, rt_size_t size, rt_uint8_t flag);
+rt_err_t rt_mb_delete(rt_mailbox_t mb);
+
+rt_err_t rt_mb_send(rt_mailbox_t mb, rt_uint32_t value);
+rt_err_t rt_mn_send_wait(rt_mailbox_t mb,
+                         rt_uint32_t  value,
+                         rt_int32_t   timeout);
+rt_err_t rt_mb_recv(rt_mailbox_t mb, rt_uint32_t *value, rt_int32_t timeout);
+rt_err_t rt_mb_control(rt_mailbox_t mb, int cmd, rt_int32_t *arg);
+#endif /* end of RT_USING_MAILBOX */
+
+#ifdef RT_USING_MESSAGEQUEUE
+rt_err_t rt_mq_init(rt_mq_t     mq,
+                    const char *name,
+                    void       *msgpool,
+                    rt_size_t   msg_size,
+                    rt_size_t   pool_size,
+                    rt_uint8_t  flag);
+rt_err_t rt_mq_detach(rt_mq_t mq);
+rt_mq_t rt_mq_create(const char *name,
+                     rt_size_t   msg_size,
+                     rt_size_t   max_msgs,
+                     rt_uint8_t  flag);
+rt_err_t rt_mq_delete(rt_mq_t mq);
+
+rt_err_t rt_mq_send(rt_mq_t mq, void *buffer, rt_size_t size);
+rt_err_t rt_mq_urgent(rt_mq_t mq, void *buffer, rt_size_t size);
+rt_err_t rt_mq_recv(rt_mq_t    mq,
+                    void      *buffer,
+                    rt_size_t  size,
+                    rt_int32_t timeout);
+rt_err_t rt_mq_control(rt_mq_t mq, int cmd, void *arg);
+#endif /* end of RT_USING_MESSAGEQUEUE */
+
+/**@}*/
